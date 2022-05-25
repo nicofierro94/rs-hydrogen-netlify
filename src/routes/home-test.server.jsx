84b60ae2ed5eff8b33d8fc8844cb6cbcdda6a4a9
Nicolas Fierro <nicolasfierro@192.168.1.2>
2022-05-25 17:19:46 -0300

    add home page

---
+++ src/routes/home-test.server.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout.server";
+
+export default function HomeTest() {
+    return (
+        <Layout>
+            <h1>Acá va la home Gabi. El Layout que envuelve lo que hace es ponerte el header y el footer, si se lo sacas te queda blanca la pagina</h1>
+        </Layout>
+    )
+}
