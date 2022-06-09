376e05cd76ac24af0b8c36344f7b9ebdfa487b09
Gabriel <gabriel@retailsystem.com>
2022-06-09 11:56:16 -0300

    9/6

--- src/rs-components/FilterModal.client.jsx
+++ src/rs-components/FilterModal.client.jsx
@@ -1,4 +1,5 @@
 import ArrowRightIcon from "../assets/icons/ArrowRightIcon"
+import SettingsIcon from "../assets/icons/SettingsIcon"
 import { useState } from "react";
 
 export default function CustomizeModal({ show, close }) {
@@ -13,9 +14,42 @@ export default function CustomizeModal({ show, close }) {
             <div className={`customize-modal ${show && 'show'}`}>
                 <div className="content">
                     <span className="back" onClick={clickClose}><ArrowRightIcon /></span>
+                </div>
+                <div className="filterSection">
+                    <div className="filterSection__container">
+                        <div className="filterSection__top-area">
+                            <div className="SectionProductTop__category-name">
+                                <h1>Filters</h1>
+                            </div>
+                            <div className="ProductList__clear-button">
+                                <button type="button">Clear All </button>
+                            </div>
+                        </div>  
+                        <div className="filterSection__filter-group">
 
+                            filtros              
+
+                            
+
+                        </div> 
+                        <div className="filterSection__bottom-area">
+                            <div className="filterSection__custimizable">                                
+                                <div className="optionTitle">
+                                    <p>Customizable<span>(230)</span></p>
+                                </div>
+                                <div className="icon-customized">
+                                    <span><SettingsIcon /></span>
+                                </div>
+                            </div>
+                            <div className="filterSection__quickShip">
+
+                            </div>
+
+                        </div>                      
+                    </div>
                 </div>
             </div>
         </div>
     )
+    
 }
